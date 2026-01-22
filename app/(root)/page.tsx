1ee5407e4a5f5ed0b8bf4cd066d476ac59c63f7f
Kelia Simbi <simbikelia@gmail.com>
2026-01-22 11:36:15 +0200

    - Removed default Home page and replaced it with a new admin dashboard.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <div>
+    <p>Hello admin dashboard</p>
+    <Button size="lg" variant="link">Click me</Button>
+   </div>
+  );
+}

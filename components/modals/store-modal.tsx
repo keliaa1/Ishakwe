986d29b3259e248eeb384bdb314328bb1e290e98
Kelia Simbi <simbikelia@gmail.com>
2026-01-22 22:02:43 +0200

    Add StoreModal component for creating new stores

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,15 @@
+"use client";
+ import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+ export const StoreModal = ()=>{
+    const storeModal = useStoreModal();
+    <Modal
+    title="Create Store"
+    description="Add a new store to manage products and categories"
+    isOpen={storeModal.isOpen}
+    onClose={storeModal.onClose}
+    >
+        Futue Create store form
+    </Modal>
+ }
